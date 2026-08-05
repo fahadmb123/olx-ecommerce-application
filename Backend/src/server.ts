0e15a019623f5a6be30a59626763c6857a10fb5a
Fahad MB <fahadmbasheer@gmail.com>
2026-08-05 22:37:45 +0530

    backend : Server running

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,9 +1,15 @@
 import connectDB from "./config/db";
 import * as dotenv from "dotenv";
+import {app} from "./app"
 
-
+const PORT = process.env.PORT || 5000
 
 
 
 dotenv.config()
 connectDB()
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running on PORT ${PORT} localhost:${PORT}`)
+})
