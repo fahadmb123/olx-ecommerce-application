fbe8328912d8db759fedb2965855fcd338acee49
Fahad MB <fahadmbasheer@gmail.com>
2026-08-06 21:58:19 +0530

    Backend : Updated

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,13 +1,15 @@
 import express,{Request,Response} from "express";
 import authRouter from "./routes/auth"
 import cors from "cors"
+import errorMiddleware from "./middleware/errorMiddleware";
 
 
 
 const app = express()
+
+app.use(errorMiddleware)
 app.use(cors())
 app.use(express.json())
-
 app.use("/api/auth", authRouter);
 
 
