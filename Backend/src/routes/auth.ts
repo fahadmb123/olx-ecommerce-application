3ee6ee62a0581e2dd75f930740ff8dbdb9025dca
Fahad MB <fahadmbasheer@gmail.com>
2026-08-06 20:36:51 +0530

    Backend : Validation Middleware added

--- Backend/src/routes/auth.ts
+++ Backend/src/routes/auth.ts
@@ -1,10 +1,15 @@
 import express from "express"
 import { signup } from "../controllers/auth/controller"
+import { signupSchema } from "../validation/authSchema"
+import validate from "../middleware/validate"
 const Router = express.Router()
 
 
 
-Router.post("/signup",signup)
+Router.post("/signup",validate(signupSchema),signup)
+
+
+
 
 
 export default Router
---
+++ Backend/src/validation/authSchema.ts
@@ -0,0 +1,29 @@
+import {z} from "zod"
+
+
+export const signupSchema = z.object({
+
+    name: z
+      .string()
+      .trim()
+      .min(3, "Email is required At least 3 letters"),
+
+    email: z
+      .string()
+      .trim()
+      .min(1, "Email is required")
+      .email("Invalid email address"),
+
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(6, "Password must be at least 6 characters"),
+
+    confirmPassword: z
+      .string()
+      .min(1, "Please confirm your password"),
+})
+.refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords do not match",
+  path: ["confirmPassword"],
+});
