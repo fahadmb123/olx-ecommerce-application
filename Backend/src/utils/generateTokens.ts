85fa061a7fa0d1bf48b8193b8d49625582f3efaa
Fahad MB <fahadmbasheer@gmail.com>
2026-08-07 01:30:34 +0530

    Backend : utils generateTokens added

---
+++ Backend/src/utils/generateTokens.ts
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId: string,email: string): string => {
+    return jwt.sign(
+        {userId,email},process.env.JWT_SECRET as string,
+        { expiresIn: process.env.JWT_EXPIRES_IN as any}
+    )
+};
