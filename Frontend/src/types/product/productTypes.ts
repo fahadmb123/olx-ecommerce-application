b51e6fe28bb6658663e39ce0c8538d7c52975ee4
Fahad MB <fahadmbasheer@gmail.com>
2026-08-08 11:14:13 +0530

    Frontend : Product Redux Implimented

--- Frontend/src/types/product/productTypes.ts
+++ Frontend/src/types/product/productTypes.ts
@@ -1,6 +1,13 @@
 export type Product = {
-    title: string;
-    description: string;
-    price: number;
-    image: FileList;
+    title: string | null;
+    description: string | null;
+    price: number | null;
+    image: string | null;
+    userId:string | null;
+    category : string | null;
+}
+
+export type ProductSliceInitialType = {
+    products : Product[];
+    loading : boolean
 }
